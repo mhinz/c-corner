504af581a643bb304cfba5a2de5022a4864a2347
Marco Hinz <mh.codebro@gmail.com>
2013-12-12 23:56:07 +0100

    Update: pm.c

--- pattern matching/pm.c
+++ pattern matching/pm.c
@@ -10,16 +10,23 @@ enum shapes {
 typedef struct {
     enum shapes type;
     union {
-        struct { int width, height; } rdata;
-        struct { int radius;        } cdata;
-        struct { int side;          } sdata;
+        struct {
+            int width;
+            int height;
+        } rdata;
+        struct {
+            int radius;
+        } cdata;
+        struct {
+            int side;
+        } sdata;
     } data;
 } shape_t;
 
 double area(shape_t *s) {
     switch (s->type) {
         case Rectangle: return (s->data.rdata.width * s->data.rdata.height);
-        case Circle:    return (3.14 * s->data.cdata.radius * s->data.cdata.radius);
+        case Circle:    return (M_PI * s->data.cdata.radius * s->data.cdata.radius);
         case Square:    return (s->data.sdata.side * s->data.sdata.side);
     }
     return 0;
@@ -27,6 +34,6 @@ double area(shape_t *s) {
 
 int main(void) {
     printf("Rectangle: %f\n", area(&(shape_t){Rectangle, {3, 4}}));
-    printf("Rectangle: %f\n", area(&(shape_t){Circle,    {5}}   ));
-    printf("Rectangle: %f\n", area(&(shape_t){Square,    {5}}   ));
+    printf("Circle:    %f\n", area(&(shape_t){Circle,    {5   }}));
+    printf("Square:    %f\n", area(&(shape_t){Square,    {5   }}));
 }
